5529432e8e380742a34712ec1ef65e97d9084b13
Harsh-Topi <topiwalah@hotmail.com>
2020-08-13 16:25:01 -0400

    added _app.js for global style sheets

---
+++ pages/_app.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+// STYLESHEETS
+import 'antd/dist/antd.css';
+import '../styles/index.css';
+
+MyApp.propTypes = {
+	Component: PropTypes.any,
+	pageProps: PropTypes.any,
+};
+
+export default function MyApp({ Component, pageProps }) {
+	return (
+		<>
+			<Component {...pageProps} />
+		</>
+	);
+}
